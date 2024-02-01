8068e0187d84f5e0268aaf4b4f87acab0695b02e
Manuu500 <espartanmanu1122@gmail.com>
2024-02-01 10:44:32 +0100

    feat(Fixed page structure)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
